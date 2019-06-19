19ffebc2782b573425d792c698494beaad2550ad
alex <alejandrogarescribano@gmail.com>
2019-06-19 09:47:36 +0200

    Primer commit de Ejercicios de Java

---
+++ 801_Git_Ejercicios/src/com/basico/Operadores.java
@@ -0,0 +1,20 @@
+package com.basico;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n1=8;
+		int n2=2;
+		
+		System.out.println("la suma es " +(n1+n2));
+		System.out.println("la resta es " +(n1-n2));
+		System.out.println("la multiplicación es " +(n1*n2));
+		System.out.println("la división es " +(n1/n2));
+		System.out.println("el resto es " +(n1%n2));
+
+		
+	}
+
+}
